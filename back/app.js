ed94c0b2a19bc3918d9de80ece912236d5218b0d
SophiaOgg <120841884+SophiaOgg@users.noreply.github.com>
2023-05-03 10:02:22 +0200

    back

---
+++ back/app.js
@@ -0,0 +1,47 @@
+const express = require('express');
+
+const app = express();
+
+app.use(express.json());
+
+const Sauce = require('./models/sauce');
+
+const mongoose = require('mongoose');
+const sauce = require('./models/sauce');
+
+mongoose.connect('mongodb+srv://SosoOgg:ssaogggirlspower@cluster0.dpm0pif.mongodb.net/Piiquante?retryWrites=true&w=majority',
+  { useNewUrlParser: true,
+    useUnifiedTopology: true })
+  .then(() => console.log('Connexion à MongoDB réussie !'))
+  .catch(() => console.log('Connexion à MongoDB échouée !'));
+
+app.use((req, res, next) => {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content, Accept, Content-Type, Authorization');
+  res.setHeader('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE, PATCH, OPTIONS');
+  next();
+});
+
+app.post('/api/stuff', (req, res, next) => {
+  delete req.body._id;
+  const thing = new Thing({
+    ...req.body
+  });
+  thing.save()
+    .then(() => res.status(201).json({ message: 'Objet enregistré !'}))
+    .catch(error => res.status(400).json({ error }));
+});
+
+app.use('/api/stuff', (req, res, next) => {
+  Sauce.find()
+    .then(things => res.status(200).json(things))
+    .catch(error => res.status(400).json({ error }));
+});
+
+module.exports = app;
+
+app.get('/api/stuff/:id', (req, res, next) => {
+  Sauce.findOne({ _id: req.params.id })
+    .then(thing => res.status(200).json(thing))
+    .catch(error => res.status(404).json({ error }));
+});
